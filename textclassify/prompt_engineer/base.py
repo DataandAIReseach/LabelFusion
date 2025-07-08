dea99079b301f51fe1f170910a32fb47fcac49a3
MichSc33 <michael.schlee@uni-goettingen.de>
2025-07-08 18:28:32 +0200

    feat(.textclassify/prompt_engineer/base): first draft of pgenerate_role_prompt and generate_theory_prompt is implemented

--- textclassify/prompt_engineer/base.py
+++ textclassify/prompt_engineer/base.py
@@ -1,11 +1,15 @@
-class prompt_engineer:
+from prompt_engineer import role_prompt, theory_background, procedure_prompt, train_data_intro_prompt, answer_format_prompt
+
+
+class PromptEngineer:
+
     """
     Base class for prompt engineering.
     This class provides a template for creating prompt engineers.
     """
 
     def __init__(self, model_name: str):
-        self.model_name = model_name
+        self.full_prompt = ""
 
     def generate_prompt_single_label(self, input_data: str) -> str:
         """
@@ -20,6 +24,21 @@ class prompt_engineer:
         This method should be overridden by subclasses.
         """
         raise NotImplementedError("Subclasses should implement this method.")
+    
+    def generate_theory_prompt(self) -> None:
+        theory_background="""
+            Given the following dataset, generate a theory prompt. As an example the following theory prompt for stereotypical and hegemonic masculinity is given:
+            {theory_background}
+        """
+        self.full_prompt += theory_background
+    
+    def generate_role_prompt(self) -> None:
+        role_prompt = """
+        Given the following dataset, generate a role prompt. As an example the following role prompt for stereotypical and hegemonic masculinity is given:
+        {role_prompt}
+        """
+        self.full_prompt += role_prompt
+
     
     
     
